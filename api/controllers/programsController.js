9010ff539cf0775450d2b31aa7bababf04f608e9
NicklasNielsen <you@example.com>
2018-10-11 21:50:05 +0200

    added jwt validation

--- api/controllers/programsController.js
+++ api/controllers/programsController.js
@@ -1,8 +1,14 @@
 var db = require('../mongoHandler');
-var jwtDecode = require('jwt-decode');
+var getPem = require('rsa-pem-from-mod-exp')
+const ax = require('axios');
+var jwt = require('jsonwebtoken');
+
+var googleVerificationSite = 'https://accounts.google.com/.well-known/openid-configuration';
+var modulus = '';
+var exponent = '';
 
 module.exports.programs = async (req, res) => {
-    var id = getIdFromToken(req.headers.authorization)
+    var id = await getIdFromToken(req.headers.authorization)
     if(id == null){
         res.status(401);
         res.json("No Token given");
@@ -25,33 +31,38 @@ module.exports.dropCollection = async (req, res) => {
 };
 
 module.exports.removeProgram = async (req, res) => {
-    var id = getIdFromToken(req.headers.authorization)
+    var id = await getIdFromToken(req.headers.authorization)
     var data = await db.removeProgram(id, req.params.programId);
     returnStatus(data, res, 200)
 };
 
 module.exports.updateLogged = async (req, res) => {
-    var id = getIdFromToken(req.headers.authorization)
+    var id = await getIdFromToken(req.headers.authorization)
     var data = await db.updateLogged(id, req.body.Logged, req.body.programId, req.body.exerciseId);
     returnStatus(data, res, 200)
 };
 
 module.exports.AddExerciseToProgram = async (req, res) => {
-    var id = getIdFromToken(req.headers.authorization)
+    var id = await getIdFromToken(req.headers.authorization)
     var data = await db.AddExerciseToProgram(id, req.body.exercise, req.body.programId);
     returnStatus(data, res, 201)
 };
 
 module.exports.RemoveExerciseFromProgram = async (req, res) => {
-    var id = getIdFromToken(req.headers.authorization)
+    var id = await getIdFromToken(req.headers.authorization)
     var data = await db.RemoveExerciseFromProgram(id, req.body.exerciseId, req.body.programId);
     returnStatus(data, res, 200)
 };
 
-function getIdFromToken(token){
-    var decoded = jwtDecode(token);
-    console.log(decoded);
-    return decoded.sub
+async function getIdFromToken(token){
+    let decodedToken;
+    await retrieveModulusAndExponent();
+    jwt.verify(token, getPem(this.modulus, this.exponent), (err, decode) => {
+        if(err) console.log(err);
+        decodedToken = decode;
+    })
+    console.log(decodedToken);
+    return decodedToken.sub
 }
 
 function returnStatus(data, res, successCode){
@@ -62,4 +73,11 @@ function returnStatus(data, res, successCode){
         res.status(401);
         res.json("Token does not give access to the wanted resource");
     }
+}
+
+async function retrieveModulusAndExponent(){
+    var googleUrl = (await ax.get(googleVerificationSite)).data.jwks_uri;
+    var data = (await ax.get(googleUrl)).data;
+    this.modulus = data.keys[1].n;
+    this.exponent = data.keys[1].e;
 }
